8a8a6e2eed8406ae4616827714a132604cf105b8
tweiiy <tweiiymail@gmail.com>
2023-03-06 14:56:51 +0300

    positiveSum method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,23 @@ public class Main {
     public static void print(String firstWord, String secondWord, String thirdWord){
         System.out.println(firstWord);
         System.out.println(secondWord);
-        System.out.print(thirdWord);
+        System.out.println(thirdWord);
+    }
+
+    public static void positiveSum(int firstInt, int secondInt){
+        if(firstInt+secondInt>=0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
     }
 
     public static void main(String[] args) {
+        System.out.println("Первый пункт:");
         print("Привет", "мир", "!");
+        System.out.println("Второй пункт:");
+        System.out.println("Первое число: 5, второе число: 10");
+        positiveSum(5, 10);
+        System.out.println("Первое число: 5, второе число: -10");
+        positiveSum(5, -10);
     }
 }
