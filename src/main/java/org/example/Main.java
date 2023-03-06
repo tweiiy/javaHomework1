f6fb13616c311b81691ef4b35ba097e1692a3a42
tweiiy <tweiiymail@gmail.com>
2023-03-06 14:43:23 +0300

    print method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+    public static void print(String firstWord, String secondWord, String thirdWord){
+        System.out.println(firstWord);
+        System.out.println(secondWord);
+        System.out.print(thirdWord);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        print("Привет", "мир", "!");
     }
 }
