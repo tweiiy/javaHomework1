bb35fb0739dc0d2caf07f8290f403d0628c9aaa3
tweiiy <tweiiymail@gmail.com>
2023-03-06 15:07:36 +0300

    trafficLight method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,13 +14,37 @@ public class Main {
             System.out.println("Сумма отрицательная");
     }
 
+    public static void trafficLight(int a){
+        int lightInt = a;
+        if(lightInt<=0)
+            System.out.println("Красный");
+        else if(lightInt>0&&lightInt<=100)
+            System.out.println("Желтый");
+        else
+            System.out.println("Зеленый");
+    }
+
     public static void main(String[] args) {
         System.out.println("Первый пункт:");
         print("Привет", "мир", "!");
-        System.out.println("Второй пункт:");
+
+        System.out.println("\nВторой пункт:");
         System.out.println("Первое число: 5, второе число: 10");
         positiveSum(5, 10);
         System.out.println("Первое число: 5, второе число: -10");
         positiveSum(5, -10);
+
+        System.out.println("\nТретий пункт:");
+        System.out.println("Число: -3");
+        trafficLight(-3);
+        System.out.println("Число: 0");
+        trafficLight(0);
+        System.out.println("Число: 3");
+        trafficLight(3);
+        System.out.println("Число: 100");
+        trafficLight(100);
+        System.out.println("Число: 103");
+        trafficLight(103);
+
     }
 }
