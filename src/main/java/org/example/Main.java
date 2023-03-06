8b151e0c4e5eee636e0126c44d5db01d2c7cafe5
tweiiy <tweiiymail@gmail.com>
2023-03-06 15:20:28 +0300

    comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,20 @@
 package org.example;
 
 public class Main {
-    public static void print(String firstWord, String secondWord, String thirdWord){
+    public static void print(String firstWord, String secondWord, String thirdWord){ // Первый пункт
         System.out.println(firstWord);
         System.out.println(secondWord);
         System.out.println(thirdWord);
     }
 
-    public static void positiveSum(int firstInt, int secondInt){
+    public static void positiveSum(int firstInt, int secondInt){ // Второй пункт
         if(firstInt+secondInt>=0)
             System.out.println("Сумма положительная");
         else
             System.out.println("Сумма отрицательная");
     }
 
-    public static void trafficLight(int a){
-        int lightInt = a;
+    public static void trafficLight(int lightInt){ // Третий пункт
         if(lightInt<=0)
             System.out.println("Красный");
         else if(lightInt>0&&lightInt<=100)
@@ -24,7 +23,7 @@ public class Main {
             System.out.println("Зеленый");
     }
 
-    public static void comparison(int firstInt, int secondInt){
+    public static void comparison(int firstInt, int secondInt){ // Четвертый пункт
         if(firstInt>secondInt)
             System.out.println("Первое число больше второго");
         else if(firstInt<secondInt)
@@ -34,16 +33,16 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Первый пункт:");
+        System.out.println("Первый пункт:"); // Создать метод который при вызове печатает 3 слова в столбец
         print("Привет", "мир", "!");
 
-        System.out.println("\nВторой пункт:");
+        System.out.println("\nВторой пункт:"); // Создать метод, который суммирует 2 переменные. Если >=0 то выводится "сумма положительная" (отрицательная если <0)
         System.out.println("Первое число: 5, второе число: 10");
         positiveSum(5, 10);
         System.out.println("Первое число: 5, второе число: -10");
         positiveSum(5, -10);
 
-        System.out.println("\nТретий пункт:");
+        System.out.println("\nТретий пункт:");// Если переменная <=0, то в "красное", если от 0 до 100(включительно) "желтый", если больше 100 то "зеленый"
         System.out.println("Число: -3");
         trafficLight(-3);
         System.out.println("Число: 0");
@@ -55,7 +54,7 @@ public class Main {
         System.out.println("Число: 103");
         trafficLight(103);
 
-        System.out.println("\nЧетвертый пункт:");
+        System.out.println("\nЧетвертый пункт:"); // Сравнение двух значений
         System.out.println("Первое число: 2, второе число: 1");
         comparison(2, 1);
         System.out.println("Первое число: 4, второе число: 5");
